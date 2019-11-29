5d38b752b6b72dc21330414697f115d5a1a4b9a1
karenmanzur <karenmanzur@users.noreply.github.com>
2019-11-29 12:02:16 -0500

    modal Enviar Cupón

--- database.php
+++ database.php
@@ -7,7 +7,7 @@
  try {
      $db = new Medoo([
      'database_type' => 'mysql',
-     'database_name' => 'damda',
+     'database_name' => 'dmcFinal',
      'server' => 'smoothoperators.com.mx',
      'username' => 'remote',
      'password' => 'D7AC3D58A7318',
