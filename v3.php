a8388b0ee350ac4450c75d5197918bb6886f5f1a
karenmanzur <karenmanzur@users.noreply.github.com>
2019-11-29 22:58:26 -0500

    correo

--- v3.php
+++ v3.php
@@ -1,41 +1,16 @@
 <?php
-require 'vendor/phpmailer/phpmailer/PHPMailerAutoload.php';
- $mail = new PHPMailer;
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $phone = $_POST['phone'];
-    $where = $_POST['where'];
-    $ciudad = $_POST['ciudad'];
-    $estado = $_POST['estado'];
-    $message = $_POST['message'];
- $mail->setFrom('contacto@smoothoperators.com.mx', 'Damda');
- $mail->addAddress('k.manzur13@gmail.com', 'Administrador');
- $mail->addAddress('max_8_1994@hotmail.com', 'Administrador');
- $mail->isHTML(true); 
- $mail->CharSet = 'UTF-8';
- 
- $mail->Subject = 'Nueva solicitud de contacto Damda:';
- 
- $mail->Body    = '<strong> Nuevo contaco de</strong><br>
-                Email: ('.$email.')';
- $mail->AltBody = $mensaje;
- $mail->Body    = 'Nuevo mensaje de contacto de Damda por parte de: <strong>'
-    .$name.'  ('.$email.')</strong><br>El mensaje es el siguiente<br><br><strong>
-    Nombre: '.$name.'<br>
-    Correo: '.$email.'<br>
-    Teléfono: '.$phone.'<br>
-    ¿Dondé nos encontro?: '.$where.' <br>
-    Ciudad: '.$ciudad.' <br>
-    Estado: '.$estado.' <br>
-    Mensaje: '.$message.'</strong><br><br> 
-    Contactar a la brevedad posible';
-    $mail->AltBody = $mensaje;
- 
- 
- if(!$mail->send()) {
-     $resp = 'ERROR';
- } else {
-     $resp = 'OKAY';
- }
- echo json_encode( $resp );
+// require 'vendor/phpmailer/phpmailer/PHPMailerAutoload.php';
+$destino="k.manzur13@gmail.com";
+$nombre=$_POST["name"];
+$email=$_POST["email"];
+$telefono=$_POST["phone"];
+$where=$_POST["where"];
+$ciudad=$_POST["ciudad"];
+$estado=$_POST["estado"];
+$message=$_POST["message"];
+
+$contenido="Nombre: ".$nombre."\nCorreo: ".$email."\nTelefono: ".$telefono."¿Dondé nos encontraste?: ".$where."Ciudad: ".$ciudad."Estado: ".$estado."\nComentario: ".$message;
+mail($destino,"Contacto", $contenido);
+header("Location: index.html");
+
 ?>
