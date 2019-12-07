4ba62af4e8ca9fb09a4987b442d99bebb0c74f10
karenmanzur <karenmanzur@users.noreply.github.com>
2019-12-06 20:06:05 -0500

    correo

--- v3.php
+++ v3.php
@@ -4,12 +4,12 @@ $destino="k.manzur13@gmail.com";
 $nombre=$_POST["name"];
 $email=$_POST["email"];
 $telefono=$_POST["phone"];
-$where=$_POST["where"];
-$ciudad=$_POST["ciudad"];
-$estado=$_POST["estado"];
+// $where=$_POST["where"];
+// $ciudad=$_POST["ciudad"];
+// $estado=$_POST["estado"];
 $message=$_POST["message"];
 
-$contenido="Nombre: ".$nombre."\nCorreo: ".$email."\nTelefono: ".$telefono."¿Dondé nos encontraste?: ".$where."Ciudad: ".$ciudad."Estado: ".$estado."\nComentario: ".$message;
+$contenido="Nombre: ".$nombre."\nCorreo: ".$email."\nTelefono: ".$telefono."\nComentario: ".$message;
 mail($destino,"Contacto", $contenido);
 header("Location: index.html");
 
