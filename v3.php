f8c76d3965e125c656e1af7489e3c7740fc49a75
karenmanzur <karenmanzur@users.noreply.github.com>
2019-12-10 19:16:01 -0500

    Revert "correos"
    This reverts commit b2d636a4e63f64952466ecccd78c2dfb466519fa.

--- v3.php
+++ v3.php
@@ -1,34 +1,16 @@
 <?php
-require 'vendor/phpmailer/phpmailer/PHPMailerAutoload.php';
+// require 'vendor/phpmailer/phpmailer/PHPMailerAutoload.php';
+$destino="k.manzur13@gmail.com";
+$nombre=$_POST["name"];
+$email=$_POST["email"];
+$telefono=$_POST["phone"];
+// $where=$_POST["where"];
+// $ciudad=$_POST["ciudad"];
+// $estado=$_POST["estado"];
+$message=$_POST["message"];
 
-    $mail = new PHPMailer;
-
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $phone = $_POST['phone'];
-    $message = $_POST['message'];
-        
-    $mail->setFrom('k.manzur13@gmail.com', 'Contacto');
-    $mail->addAddress('luisfertg_016@hotmail.com', 'Administrador');
-    // $mail->addAddress('@gmail.com', 'Administrador');
-         // Add a recipient
-    
-    $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-    $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
-    $mail->isHTML(true);                                  // Set email format to HTML
-    $mail->CharSet = 'UTF-8';
-    
-    $mail->Subject = 'Nueva solicitud de contacto Damda';
-    
-    $mail->Body    = 'Nuevo mensaje de contacto de Damda por parte de: <strong>'
-    .$name.'  ('.$email.')</strong><br>El mensaje es el siguiente<br><br><strong>
-    Nombre: '.$name.'<br>
-    Correo: '.$email.'<br>
-    Teléfono: '.$phone.'<br>
-    Mensaje: '.$message.'</strong><br><br> 
-    Contactar a la brevedad posible';
-
-    $mail->AltBody = $mensaje;
-    
+$contenido="Nombre: ".$nombre."\nCorreo: ".$email."\nTelefono: ".$telefono."\nComentario: ".$message;
+mail($destino,"Contacto", $contenido);
+header("Location: index.php");
 
 ?>
